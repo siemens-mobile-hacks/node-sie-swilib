2baba4eded178e03afa837f3508d89b8a176d0cc
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-06-04 09:16:48 +0000

    fixes

--- src/index.js
+++ src/index.js
@@ -382,7 +382,7 @@ function parseSwilibFuncName(comm) {
 	.replace(/\/\/.*?$/i, '')
 	.replace(/(;|\*NEW\*|\?\?\?)/gi, '')
 	.replace(/Run ScreenShooter on function /g, '')
-	.replace(/\((API|MP)\)/, '') // thanks dimonp25
+	.replace(/\((API|MP|Disp)\)/, '') // thanks dimonp25
 	.replace(/С/gi, 'C')
 	.trim();
 
